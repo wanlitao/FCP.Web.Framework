2c2e8156736933bbfa7f9c9b72ed65c566340362
万里涛 <wanlitao_3055601@outlook.com>
2017-07-05 19:46:12 +0800

    owinhost add health status extension

---
+++ src/FCP.Web.OwinHost/Extensions/HealthStatusAppBuilderExtensions.cs
@@ -0,0 +1,29 @@
+﻿using FCP.Util;
+using Owin;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace FCP.Web.OwinHost
+{
+    public static class HealthCheckAppBuilderExtensions
+    {
+        public static IAppBuilder UseHealthStatus(this IAppBuilder app, string statusRoute)
+        {
+            if (statusRoute.isNullOrEmpty())
+                throw new ArgumentNullException(nameof(statusRoute));
+
+            var mapRoutePath = "/" + statusRoute.TrimStart('/');
+
+            app.Map(mapRoutePath,
+                appBuilder => appBuilder.Run(ctx =>
+                {
+                    ctx.Response.StatusCode = (int)HttpStatusCode.OK;
+                    return Task.FromResult(0);
+                })
+            );
+
+            return app;
+        }
+    }
+}
